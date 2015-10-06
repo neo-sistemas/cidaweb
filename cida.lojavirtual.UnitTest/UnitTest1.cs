7103e45c1957abbd42f5551cfa1907a4a363bb8d
neo-sistemas <neosistemas@outlook.com>
2015-10-06 00:25:04 -0300

    Versão Beta 1.0 - 06-10-2015

---
+++ cida.lojavirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace cida.lojavirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestCida
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ cida.lojavirtual.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cida.lojavirtual.web.MvcApplication" Language="C#" %>
