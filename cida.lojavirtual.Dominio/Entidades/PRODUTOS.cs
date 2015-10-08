72ff7723f0a25b2182ceed77eeb818f69f69ac1b
neo-sistemas <neosistemas@outlook.com>
2015-10-07 23:34:47 -0300

    Alteração no Dominio 07-10-15

---
+++ cida.lojavirtual.Dominio/Entidades/PRODUTOS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cida.lojavirtual.Dominio.Entidades
+{
+    class PRODUTOS
+    {
+        public int CodProduto { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Categoria { get; set; }
+    }
+}
